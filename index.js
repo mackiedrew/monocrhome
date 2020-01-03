c19ce0c0b9a0448f3f843173c245d4158c27ea2e
Mackie Drew <mackiedrew@gmail.com>
2020-01-03 03:37:07 -0500

    :sparkles: Simplified

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const applyStyles = (doc, className) => {
-  if (!doc || !doc.body || !doc.head) console.warn("Monochrome: could not make page monochrome")
-  const css = "body." + className + " { filter: grayscale(100%); }"
+const main = doc => {
+  // Ensure the document and all provided elements exist before progressing
+  if (!doc || !doc.head) {
+    return console.warn("Monochrome: could not make page monochrome")
+  }
+  
+  // Generate style tag for head
   const styleTag = doc.createElement("style")
+
+  // Add style tag to the document head
   doc.head.appendChild(styleTag)
+
+  // Create a specific class
   styleTag.type = "text/css"
-  styleTag.appendChild(document.createTextNode(css))
-  doc.body.classList.add(className)
+  styleTag.appendChild(document.createTextNode("body { filter: grayscale(100%); }"))
 }
 
-const className = "monochrome"
-applyStyles(document, className)
+// Provide document as a parameter to avoid global access in function
+main(document)
 
